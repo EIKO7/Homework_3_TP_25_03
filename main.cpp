bb5cdc61fd9f37256b02ca4e9a33f0c47581644e
EIKO7 <camper1773@mail.ru>
2026-03-26 00:38:12 +0300

    ver 2

--- main.cpp
+++ main.cpp
@@ -3,10 +3,36 @@
 template< class T > 
 size_t unique(T* a, size_t s) 
 {
-    return s;
+    if (a == 0) {
+        return 0;
+    }
+    
+    if (s <= 1) {
+        return s;
+    }
+    
+    size_t new_size = 1;  
+    
+    for (size_t i = 1; i < s; i++) {
+        bool is_duplicate = false;
+        
+        for (size_t j = 0; j < new_size; j++) {
+            if (a[i] == a[j]) {
+                is_duplicate = true;
+                break;
+            }
+        }
+        
+        if (!is_duplicate) {
+            a[new_size] = a[i];
+            new_size++;
+        }
+    }
+    
+    return new_size;
 }
 
 int main() 
 {
-    return 0;
+    
 }
